b481a6d8221271ebcfc94a2351cd5e2594c611e1
Lavermss <thxsoo@naver.com>
2026-03-25 12:42:02 +0900

    react1 03/25

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery from "./components/Gallery";
+import NamedComponentTest from "./components/NamedComponentTest";
+import Profile from "./components/Profile";
+
+
+export default function App(){
+  return(
+    <>
+    <NamedComponentTest/>
+    <Gallery/>
+    </>
+  )
+}
